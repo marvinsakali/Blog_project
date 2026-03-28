071a40438e9e45f28f8c08e81238387c9a3d27c1
marvinsakali <marvinsakali09@gmail.com>
2026-03-28 10:11:50 +0300

    added post create and update features

--- src/blog/models.py
+++ src/blog/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
-from PIL import Image
+from django.urls import reverse
 
 
 class Post(models.Model):
@@ -13,4 +13,5 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
-
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk': self.pk})
